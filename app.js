b75dd1dede0dfd4a254b653374b92929cf21efa8
austriker27 <davidalindahl@gmail.com>
2017-08-09 16:47:20 -0700

    update jss for questions 6 and 7

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// var correctAnswers = []
+
 var prediction = prompt('Welcome to the quiz about me. Do you think you know me well?');
 var predictionLower = prediction.toLowerCase();
 if (predictionLower === 'yes' || predictionLower === 'y') {
@@ -56,3 +58,39 @@ if (hardWorkingLower === 'yes' || hardWorkingLower === 'y') {
   alert('Incorrect. You need to answer Yes or No.');
 }
 console.log(hardWorkingLower);
+
+// 6th question
+for (i = 0; i < 4; i++); {
+  var statesLived = prompt('Okay, time to get serious... How many states in the USA have I been to?')
+  if (statesLived === '47') {
+    alert('Wow, that was impressive! You win!');
+    break;
+  } else if (statesLived >= '0' && statesLived < '47') {
+    alert('Nope, try a little higher.')
+  } else if (statesLived > '47' && statesLived <= '50') {
+    alert('Whoa, too high. Try guessing something lower.')
+  } else {
+    alert('Hey there, don\'t be silly. Put a number in.')
+  }
+  console.log(statesLived);
+  }
+
+// 7th question
+var statesNotLived = ['North Dakota', 'Arizona', 'Nevada']
+
+for (i = 0; i < 6, i++); {
+  var statesLivedGuess = prompt('Okay smartypants, can you guess which states I haven\'t been to?')
+  console.log(statesLivedGuess
+  if (statesLivedGuess.toLowerCase(); === statesNotLived) {
+    alert('Thats right! I have not been to' + statesNotLived '.');
+    break;
+  } else (statesLivedGuess.toLowerCase(); != statesNotLived;) {
+    alert('Nope try again')
+  }
+}
+
+
+// directions :
+// As a developer, I want to add a sixth question to my guessing game that takes numeric input by prompting a user to guess a number (for instance, "What is my favorite number?" or "How many Pokémon did I catch yesterday?"), and indicates to the user whether the guess is 'too high' or 'too low', and gives the user exactly four opportunities to get the correct answer, so that my fancy programming skills are showcased.
+// As a developer, I want to add a seventh question to my guessing game that has multiple possible correct answers that are stored in an array. For instance, "Can you guess a state that I have lived in besides Washington?", so that even more of my programming skills are showcased. For now, I will structure this question so that the user has six tries to get a single correct answer, and upon using up those tries OR getting a correct answer, displays a message to the user indicating all of the possible correct answers.
+// As a developer, I want to keep a tally of the total number of correct answers given by the user, and at the end of the game, tell the user how well they did with a personalized custom message that includes the number of correct answers and also addresses the user by name, e.g. "You got 4 out of 7 questions correct, Bobbi! Better luck next time."
