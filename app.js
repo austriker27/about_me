c6d9df085e0e7ddf518dac7057049e248f38a68f
Michael Shinners <mishinners@icloud.com>
2017-08-10 14:33:29 -0700

    working on Q7.

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 'use strict';
 
-var name = prompt('What is your name?')
+var name = prompt('What is your name?');
 
-var correctAnswers = []
+var correctAnswers = [];
 var prediction = prompt('Welcome to the quiz about me. Do you think you know me well?');
 var predictionLower = prediction.toLowerCase();
 if (predictionLower === 'yes' || predictionLower === 'y') {
   alert ('Oh really? You think so? Well good luck then.');
 } else if (predictionLower === 'no' || predictionLower === 'n') {
   alert('Bummer, this is not going to go well.');
-}  else {
+} else {
   alert('You need to answer with a yes or no.');
 }
 console.log(predictionLower);
 
 
-var learnedCSS = prompt('Did I modify CSS files many years ago before even knowing what CSS stood for?')
+var learnedCSS = prompt('Did I modify CSS files many years ago before even knowing what CSS stood for?');
 var learnedCSSLower = learnedCSS.toLowerCase();
 if (learnedCSSLower === 'yes' || learnedCSSLower === 'y') {
-  alert('Oh heck ya!')
+  alert('Oh heck ya!');
   correctAnswers.push(1);
 } else if (learnedCSSLower === 'no' || learnedCSSLower === 'n') {
   alert('False, I actually did that.');
@@ -28,10 +28,10 @@ if (learnedCSSLower === 'yes' || learnedCSSLower === 'y') {
 console.log(learnedCSSLower);
 
 
-var photoExp = prompt('Do I have years of photography experience that results in impeccable design decisions?')
+var photoExp = prompt('Do I have years of photography experience that results in impeccable design decisions?');
 var photoExp = photoExp.toLowerCase();
 if (photoExp === 'yes' || photoExp === 'y') {
-  alert('You better believe I do (although applying that design eye is still a work in progress).')
+  alert('You better believe I do (although applying that design eye is still a work in progress).');
   correctAnswers.push(1);
 } else if (photoExp === 'no' || photoExp === 'n') {
   alert('False, I do have years of photography experience.');
@@ -40,10 +40,10 @@ if (photoExp === 'yes' || photoExp === 'y') {
 }
 console.log(photoExp);
 
-var dundieWinner = prompt('Am I the receipient of multiple Dundie Awards?')
+var dundieWinner = prompt('Am I the receipient of multiple Dundie Awards?');
 var dundieWinnerLower = dundieWinner.toLowerCase();
 if (dundieWinnerLower === 'yes' || dundieWinnerLower === 'y') {
-  alert('Ding, ding, winner! I have indeed won multiple Dundees. ')
+  alert('Ding, ding, winner! I have indeed won multiple Dundees. ');
   correctAnswers.push(1);
 } else if (dundieWinnerLower === 'no' || dundieWinnerLower === 'n') {
   alert('Sorry, that is not correct. I have won many Dundies!');
@@ -52,10 +52,10 @@ if (dundieWinnerLower === 'yes' || dundieWinnerLower === 'y') {
 }
 console.log(dundieWinnerLower);
 
-var hardWorking = prompt('Legend tells a tale of a David A. Lindahl working straight for seven days without food nor rest in order to meet a deadline with a flawless, beautiful product. Is this true?')
+var hardWorking = prompt('Legend tells a tale of a David A. Lindahl working straight for seven days without food nor rest in order to meet a deadline with a flawless, beautiful product. Is this true?');
 var hardWorkingLower = hardWorking.toLowerCase();
 if (hardWorkingLower === 'yes' || hardWorkingLower === 'y') {
-  alert('Yup, the legend does not lie.')
+  alert('Yup, the legend does not lie.');
   correctAnswers.push(1);
 } else if (hardWorkingLower === 'no' || hardWorkingLower === 'n') {
   alert('You are incorrect, you must believe the legend is fact.');
@@ -66,39 +66,40 @@ console.log(hardWorkingLower);
 
 // 6th question
 for (i = 0; i < 4; i++) {
-  var statesBeen = prompt('Okay, time to get serious... How many states in the USA have I been to? You only get 4 attempts, choose wisely!')
+  var statesBeen = prompt('Okay, time to get serious... How many states in the USA have I been to? You only get 4 attempts, choose wisely!');
   if (statesBeen === '47') {
-    alert('Wow, that was impressive! You win!')
+    alert('Wow, that was impressive! You win!');
     correctAnswers.push(1);
     break;
   } else if (statesBeen >= '0' && statesBeen < '47') {
-    alert('Nope, try a little higher.')
+    alert('Nope, try a little higher.');
   } else if (statesBeen > '47') {
-    alert('Whoa, too high. Try guessing something lower.')
+    alert('Whoa, too high. Try guessing something lower.');
     // do i need to add another else if to for if i ==== 4, to end the looop after guesses??
   } else {
-    alert('Hey there, don\'t be silly. Put a number in.')
+    alert('Hey there, don\'t be silly. Put a number in.');
   }
   console.log(statesBeen);
-  }
+}
 
 
 // 7th question
-var statesNotLived = ['north dakota', 'arizona', 'nevada'];
 
 for (var i = 0; i < 6; i++) {
-  var statesLivedGuess = prompt('Okay smartypants, lets try one final question.. Which states haven\'t I been to?')
+  var statesLivedGuess = prompt('Okay smartypants, lets try one final question.. Which states haven\'t I been to?');
   console.log(statesLivedGuess);
-  if (statesLivedGuess.toLowerCase() === statesNotLived[0] || statesNotLived[1] || statesNotLived[2] || i === 6) {
+  var statesNotLived = ['north dakota', 'arizona', 'nevada'];
+  if (statesLivedGuess.includes(statesNotLived.toLowerCase())) {
+  // if (statesLivedGuess.toLowerCase() === statesNotLived[0] || statesNotLived[1] || statesNotLived[2] || i === 6) {
     alert('You win! I have not been to the states of: ' + statesNotLived[0].toString() + ', ' + statesNotLived[1].toString() + ', ' + statesNotLived[2].toString() + '.' );
     correctAnswers.push(1);
     break;
   } else {
-    alert('Negative Ghostrider!')
+    alert('Negative Ghostrider!');
   }
 }
 
-alert('Congrats, ' + name + ' you got ' + (correctAnswers.length + 1) + ' out of 7 questions correct. Come again soon!')
+alert('Congrats, ' + name + ' you got ' + (correctAnswers.length + 1) + ' out of 7 questions correct. Come again soon!');
 
 
 // directions :
